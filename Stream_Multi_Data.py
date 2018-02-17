1e28eb401ddb98e288e3458537fe128db9569157
dexterchan <dexterchan@gmail.com>
2018-02-17 12:56:42 +0800

    update

--- Stream_Multi_Data.py
+++ Stream_Multi_Data.py
@@ -1,6 +1,54 @@
 import requests
 import time
 import datetime
+import os
+import logging
+import pathlib
 
 
-currencyLst = ['bitcoin','ethereum','ripple','litecoin','eos','iota']
+
+class CoinMarketCapQuote:
+
+    def __init__(self):
+        logging.basicConfig(level=logging.DEBUG)
+        self.currencyLst = ['bitcoin','ethereum','ripple','litecoin','eos','iota']
+        self.extractionServiceURL="https://api.coinmarketcap.com/v1/ticker/"
+        self.keys = ["price_usd","24h_volume_usd","market_cap_usd","available_supply","total_supply","percent_change_1h","percent_change_24h","percent_change_7d"]
+        self.vals = [0]*(len(self.keys))
+
+        self.path="data/"
+
+        pathlib.Path(self.path).mkdir(parents=True, exist_ok=True)
+        pass
+
+    def extractCrypto(self, ccy):
+        f_name=self.path+"live.mkt."+ccy+".csv"
+        f = open(f_name,"a")
+        url = self.extractionServiceURL+ccy
+        data = requests.get(url).json()[0]
+
+
+        for d in data.keys():
+            if d in self.keys:
+                indx = self.keys.index(d)
+                self.vals[indx] = data[d]
+        for val in self.vals:
+            f.write(val+",")
+        timenow = datetime.datetime.now().strftime("%y-%m-%d-%H-%M")
+        f.write(","+timenow)
+        f.write("\n")
+        f.flush()
+        logging.debug(ccy+":"+str(self.vals)+timenow)
+        return ccy+":"+str(self.vals)+timenow
+
+
+
+quote = CoinMarketCapQuote()
+
+while (True):
+    for ccy in quote.currencyLst:
+        status=quote.extractCrypto(ccy)
+        logging.info(status)
+    time.sleep(60)
+    #quote.extractCrypto("ethereum")
+    #quote.extractCrypto("iota")
---
+++ Stream_Multi_Tweet_Sentiment.py
@@ -0,0 +1,76 @@
+import csv
+import tweepy
+import numpy as np
+from textblob import TextBlob
+import datetime
+import time
+import logging
+import pathlib
+
+class TweeterSentimentAnalyzer:
+    def __init__(self):
+        logging.basicConfig(level=logging.DEBUG)
+
+        self.currencyLst = ['bitcoin','ethereum','ripple','litecoin','eos','iota']
+        # set twitter api credentials
+        self.consumer_key= 'JbZWz1hekxkCv1sCRW6k9BrjF'
+        self.consumer_secret= 'tjZclsG6BqdGNfnUAt1wAFRzBX3PXHDGp6z54jKryVHJN3wWOo'
+        self.access_token='926643380-mTGIw7bfYqF6sxbiZqNZb0LCdBypqAtIEC4rPtll'
+        self.access_token_secret='4hetDuuv5lDtzzAPzyN1E1a5XX41bJeQNATggb0Mwqf3m'
+
+        self.path="data/"
+
+        pathlib.Path(self.path).mkdir(parents=True, exist_ok=True)
+
+
+
+        # access twitter api via tweepy methods
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_token, self.access_token_secret)
+        self.twitter_api = tweepy.API(auth)
+        pass
+
+
+    def readSentimentByTextBlob(self, ccy):
+        path = self.path+'live.tweet.'+ccy+'.csv'
+        f = open(path,"a")
+
+        # fetch tweets by keywords
+        tweets = self.twitter_api.search(q=[ccy+', price, crypto'], count=100)
+
+        # get polarity
+        polarity = self.get_TextBlob_polarity(ccy,tweets)
+        sentiment = np.mean(polarity)
+
+        # save sentiment data to csv file
+        f.write(str(sentiment))
+        f.write(","+datetime.datetime.now().strftime("%y-%m-%d-%H-%M"))
+        f.write("\n")
+        f.flush()
+
+        pass
+
+    def get_TextBlob_polarity(self,ccy,tweets):
+        # run polarity analysis on tweets
+        f1 = open(self.path+"tweet_data."+ccy+'.txt','a')
+        tweet_polarity = []
+
+        for tweet in tweets:
+            f1.write(datetime.datetime.now().strftime("%Y%m%d-%H%M")    + str(tweet.text.encode('utf8'))+'\n')
+            analysis = TextBlob(tweet.text)
+            tweet_polarity.append(analysis.sentiment.polarity)
+        f1.flush()
+        return tweet_polarity
+
+
+
+
+sentiment = TweeterSentimentAnalyzer()
+
+while (True):
+    for ccy in sentiment.currencyLst:
+        sentiment.readSentimentByTextBlob(ccy)
+        logging.info(ccy+" sentiment ok")
+    time.sleep(60)
+    #quote.extractCrypto("ethereum")
+    #quote.extractCrypto("iota")
